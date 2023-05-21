3fc4d5d9ebf1619d34e2a6d8ca5e352f27465a4a
m-triple-m <triplem656@gmail.com>
2023-05-21 10:19:37 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Home from './components/main/Home';
 import Main from './components/main';
